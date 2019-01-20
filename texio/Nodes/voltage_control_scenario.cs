6178771208c2212d70d9c6fb3126883fe747b856
y.todoroki <Todoroki.Yoshio@ap.MitsubishiElectric.co.jp>
2019-01-20 12:02:50 +0900

    add senario setting and code

--- texio/Nodes/voltage_control_scenario.cs
+++ texio/Nodes/voltage_control_scenario.cs
@@ -1,10 +1,10 @@
 using System;
+using System.Reflection;
 using Vector.Tools;
 using Vector.CANoe.Runtime;
 using Vector.CANoe.Sockets;
 using Vector.CANoe.Threading;
 using Vector.Diagnostics;
-
 
 
 public class voltage_control_scenario : MeasurementScript
@@ -29,4 +29,17 @@ public class voltage_control_scenario : MeasurementScript
     {
     
     }
+
+[OnCANFrame(1, 0x123)] // [OnCANFrame(byte channnel, int32 id)] @ OnCANFrameAttribute Constructor (Byte, Int32) 
+    public void CANFrameReceived(CANFrame frame)
+    {
+        // Vector.Tools.Output.WriteLine(MethodBase.GetCurrentMethod().Name);
+        if(frame.Bytes[0]==0x1)
+        {
+            Vector.Tools.Output.WriteLine("0x123 recevied byte[0]:"+frame.Bytes[0].ToString("X2"));
+        }
+    }
 }
+
+// 書式を指定して数値を文字列に変換する - .NET Tips (VB.NET,C#...)
+// https://dobon.net/vb/dotnet/string/inttostring.html
