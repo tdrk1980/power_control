fd2a7b727df0ac8aae3118df52bd9d0e0206ce02
y.todoroki <Todoroki.Yoshio@ap.MitsubishiElectric.co.jp>
2019-01-20 21:27:34 +0900

    add 2sec off timer

--- texio/Nodes/voltage_control_scenario.cs
+++ texio/Nodes/voltage_control_scenario.cs
@@ -11,25 +11,27 @@ using Vector.Diagnostics;
 
 public class voltage_control_scenario : MeasurementScript
 {
+    static Vector.Tools.Timer 2sec_off_tm;
 
     public override void Initialize()
     {
-    
+        2sec_off_tm = new Timer(new TimeSpan(20*1000*1000), On2secOffTimerElapsed); // 2秒後にoffする
+        // public TimeSpan (long ticks); tickの単位は 100 nano-sec [ref4]
     }
     
     public override void Start()
     {
-    
+
     }
     
     public override void Stop()
     {
-    
+
     }
     
     public override void Shutdown()
     {
-    
+        
     }
 
     // CANframeの内容を出力する (デバッグ用)
@@ -45,12 +47,33 @@ public class voltage_control_scenario : MeasurementScript
         );
     }
 
-// CAN ID:0x123受信ハンドラ
-[OnCANFrame(1, 0x123)] // [OnCANFrame(byte channnel, int32 id)] @ OnCANFrameAttribute Constructor (Byte, Int32) 
+/////
+// ユーザ定義イベントハンドラ
+/////
+    /////
+    // 2secタイマ経過ハンドラ
+    private static void On2secOffTimerElapsed(object sender, ElapsedEventArgs e)
+    {
+        Texio.RequestVoltage.Value = 0.0;
+        2sec_off_tm.Stop();
+    }
+
+    /////
+    // CAN ID:0x123受信ハンドラ
+    [OnCANFrame(1, 0x123)] // [OnCANFrame(byte channnel, int32 id)] @ OnCANFrameAttribute Constructor (Byte, Int32) 
     public void CANFrameReceived(CANFrame frame)
     {
-
-        printCANFrame(frame);
+        // printCANFrame(frame);
+        if(frame.Bytes[0] == 0x0)
+        {
+            2sec_off_tm.Stop();
+            2sec_off_tm.Start();
+        }
+        else
+        {
+            2sec_off_tm.Stop();
+            Texio.RequestVoltage.Value = 13.0;
+        }
 
     }
 }
@@ -64,3 +87,8 @@ public class voltage_control_scenario : MeasurementScript
 
 // [ref3]:書式を指定して数値を文字列に変換する - .NET Tips (VB.NET,C#...)
 // https://dobon.net/vb/dotnet/string/inttostring.html
+
+// [ref4]:TimeSpan Constructor (System) _ Microsoft Docs
+// https://docs.microsoft.com/en-us/dotnet/api/system.timespan.-ctor?view=netframework-4.7.2#System_TimeSpan__ctor_System_Int64_
+// public TimeSpan (long ticks);
+//  ticksは、100-nanosecond units単位なので、10*1000*1000 = 1sec
