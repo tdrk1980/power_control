94739837bdf72747951b427b2885af66aedeb53d
y.todoroki <Todoroki.Yoshio@ap.MitsubishiElectric.co.jp>
2019-01-20 11:37:10 +0900

    add template

---
+++ texio/Nodes/voltage_control_scenario.cs
@@ -0,0 +1,32 @@
+using System;
+using Vector.Tools;
+using Vector.CANoe.Runtime;
+using Vector.CANoe.Sockets;
+using Vector.CANoe.Threading;
+using Vector.Diagnostics;
+
+
+
+public class voltage_control_scenario : MeasurementScript
+{
+
+    public override void Initialize()
+    {
+    
+    }
+    
+    public override void Start()
+    {
+    
+    }
+    
+    public override void Stop()
+    {
+    
+    }
+    
+    public override void Shutdown()
+    {
+    
+    }
+}
